bd7de87f0167c25cec5970f07f767c55e6be2617
Brooks, Matthew <brooksm@sec.gov>
2022-07-28 09:16:53 -0600

    Updates

--- src/ts/constants/transformations/date.ts
+++ src/ts/constants/transformations/date.ts
@@ -2,6 +2,20 @@ import moment from "moment";
 import { TransformationsNumber } from "./number";
 
 export const TransformationsDate = {
+
+  eraStart: {
+    '\u4EE4\u548C': 2018,
+    '\u4EE4': 2018,
+    '\u5E73\u6210': 1988,
+    '\u5E73': 1988,
+    '\u660E\u6CBB': 1867,
+    '\u660E': 1867,
+    '\u5927\u6B63': 1911,
+    '\u5927': 1911,
+    '\u662D\u548C': 1925,
+    '\u662D': 1925
+  },
+
   gregorianDaysInMonths: [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31],
 
   sakaMonthLength: [30, 31, 31, 31, 31, 31, 30, 30, 30, 30, 30, 30],
@@ -21,7 +35,81 @@ export const TransformationsDate = {
     [2, 20, 1],
   ],
 
-  getGregorianHindiMonthNumber: {},
+  getGregorianHindiMonthNumber: {
+    '\u091C\u0928\u0935\u0930\u0940': '01',
+    '\u092B\u0930\u0935\u0930\u0940': '02',
+    '\u092E\u093E\u0930\u094D\u091A': '03',
+    '\u0905\u092A\u094D\u0930\u0948\u0932': '04',
+    '\u092E\u0908': '05',
+    '\u091C\u0942\u0928': '06',
+    '\u091C\u0941\u0932\u093E\u0908': '07',
+    '\u0905\u0917\u0938\u094D\u0924': '08',
+    '\u0938\u093F\u0924\u0902\u092C\u0930': '09',
+    '\u0905\u0915\u094D\u0924\u0942\u092C\u0930': '10',
+    '\u0928\u0935\u092E\u094D\u092C\u0930': '11',
+    '\u0926\u093F\u0938\u092E\u094D\u092C\u0930': '12'
+  },
+
+  getSakaMonthNumber: {
+    'Chaitra': 1,
+    '\u091A\u0948\u0924\u094D\u0930': 1,
+    'Vaisakha': 2,
+    'Vaishakh': 2,
+    'Vai\u015B\u0101kha': 2,
+    '\u0935\u0948\u0936\u093E\u0916': 2,
+    '\u092C\u0948\u0938\u093E\u0916': 2,
+    'Jyaishta': 3,
+    'Jyaishtha': 3,
+    'Jyaistha': 3,
+    'Jye\u1E63\u1E6Dha': 3,
+    '\u091C\u094D\u092F\u0947\u0937\u094D\u0920': 3,
+    'Asadha': 4,
+    'Ashadha': 4,
+    '\u0100\u1E63\u0101\u1E0Dha': 4,
+    '\u0906\u0937\u093E\u0922': 4,
+    '\u0906\u0937\u093E\u0922\u093C': 4,
+    'Sravana': 5,
+    'Shravana': 5,
+    '\u015Ar\u0101va\u1E47a': 5,
+    '\u0936\u094D\u0930\u093E\u0935\u0923': 5,
+    '\u0938\u093E\u0935\u0928': 5,
+    'Bhadra': 6,
+    'Bhadrapad': 6,
+    'Bh\u0101drapada': 6,
+    'Bh\u0101dra': 6,
+    'Pro\u1E63\u1E6Dhapada': 6,
+    '\u092D\u093E\u0926\u094D\u0930\u092A\u0926': 6,
+    '\u092D\u093E\u0926\u094B': 6,
+    'Aswina': 7,
+    'Ashwin': 7,
+    'Asvina': 7,
+    '\u0100\u015Bvina': 7,
+    '\u0906\u0936\u094D\u0935\u093F\u0928': 7,
+    'Kartiak': 8,
+    'Kartik': 8,
+    'Kartika': 8,
+    'K\u0101rtika': 8,
+    '\u0915\u093E\u0930\u094D\u0924\u093F\u0915': 8,
+    'Agrahayana': 9,
+    'Agrah\u0101ya\u1E47a': 9,
+    'Margashirsha': 9,
+    'M\u0101rga\u015B\u012Br\u1E63a': 9,
+    '\u092E\u093E\u0930\u094D\u0917\u0936\u0940\u0930\u094D\u0937': 9,
+    '\u0905\u0917\u0939\u0928': 9,
+    'Pausa': 10,
+    'Pausha': 10,
+    'Pau\u1E63a': 10,
+    '\u092A\u094C\u0937': 10,
+    'Magha': 11,
+    'Magh': 11,
+    'M\u0101gha': 11,
+    '\u092E\u093E\u0918': 11,
+    'Phalguna': 12,
+    'Phalgun': 12,
+    'Ph\u0101lguna': 12,
+    '\u092B\u093E\u0932\u094D\u0917\u0941\u0928': 12
+  },
+
 
   getSakaToGregorian: (year: number, month: number, day: number) => {
     if (!year && !month && !day) {
@@ -316,10 +404,10 @@ export const TransformationsDate = {
       const regex =
         /^\s*([0-9\u0966-\u096F]{1,2})\s([\u0966-\u096F]{2}|[^\s0-9\u0966-\u096F]+)\s([0-9\u0966-\u096F]{2}|[0-9\u0966-\u096F]{4})\s*$/;
       const result = regex.exec(input);
-      if(result) {
+      if (result) {
         const year = TransformationsNumber.getDevanagariDigitsToNormal(result[3]);
         let month;
-        if() {
+        if () {
 
         } else {
 
